a6080acccf5ac9f08d8796e2171b963b5f6af132
Eric Kasakya <erickasakya@gmail.com>
2023-02-18 09:46:38 +0300

    Adding instructions to build and run the ml image

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+# Step 1: Build image and add a descriptive tag
+docker build --tag house_prediction_ml .
 
-# Step 1:
-# Build image and add a descriptive tag
+# Step 2: List docker images
+docker image ls
 
-# Step 2: 
-# List docker images
-
-# Step 3: 
-# Run flask app
+# Step 3: Run flask app
+docker run -p 8000:80 house_prediction_ml
