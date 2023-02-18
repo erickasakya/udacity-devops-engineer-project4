e6da2b00e5ab0933e0845ecc007b1301a4ff7440
Eric Kasakya <erickasakya@gmail.com>
2023-02-18 12:04:46 +0300

    Adding a script with a instructions to run the house prediction model in the k8s cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,14 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+# Step 1: This is your Docker ID/path
+dockerpath="erickasakya/house_prediction_ml"
 
-# Step 2
-# Run the Docker Hub container with kubernetes
+# Step 2: Run the Docker Hub container with kubernetes
+kubectl run house-prediction-ml --image $dockerpath --port=80
 
+# Step 3: List kubernetes pods
+kubectl get pods
 
-# Step 3:
-# List kubernetes pods
-
-# Step 4:
-# Forward the container port to a host
-
+# Step 4: Forward the container port to a host
+kubectl port-forward pods/house-prediction-ml 8000:80
