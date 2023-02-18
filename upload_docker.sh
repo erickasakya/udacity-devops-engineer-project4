cad468d32dd9b517506e2b83aae98260e5fed8b6
Eric Kasakya <erickasakya@gmail.com>
2023-02-18 11:33:54 +0300

    Adding a script to upload the image to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
-
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+# Step 1: Create dockerpath
+dockerpath="erickasakya/house_prediction_ml"
 
-# Step 2:  
-# Authenticate & tag
+# Step 2: Authenticate & tag
+export DOCKER_ID_USER = "erickasakya"
+docker login
+docker tag house_prediction_ml $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
-# Push image to a docker repository
+# Step 3: Push image to a docker repository
+docker push $dockerpath
